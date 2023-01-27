e5ef3d4aece3722473ad24f91ff75bf7617bc1c1
iljarotar <kontakt@ilja-rotar.de>
2023-01-27 08:10:50 +0100

    fractional and very slow frequencies possible

--- synth/synth.go
+++ synth/synth.go
@@ -1,8 +1,6 @@
 package synth
 
 import (
-	"math"
-
 	o "github.com/iljarotar/synth/oscillator"
 )
 
@@ -17,7 +15,7 @@ func (s *Synth) Initialize() {
 func (s *Synth) Play(input chan<- float32, play *bool) {
 	for *play {
 		input <- float32(s.WaveTable.SignalFunc(s.WaveTable.Phase))
-		_, s.WaveTable.Phase = math.Modf(s.WaveTable.Phase + s.WaveTable.Step)
+		s.WaveTable.Phase += s.WaveTable.Step
 	}
 	close(input)
 }
