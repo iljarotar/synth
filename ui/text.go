2e3b562b2e2243aed97c0116f65ff5d950abafc7
iljarotar <kontakt@ilja-rotar.de>
2023-01-31 13:34:33 +0100

    update menu

--- ui/text.go
+++ ui/text.go
@@ -6,6 +6,7 @@ Menu
 apply, a        reload last file
 clear, c        clear screen
 exit, e         exit synth
+help, h         print this menu
 load, l         load file
 play, p         start synth
 root, r         specify root path
