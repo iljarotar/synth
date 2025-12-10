e47ede809f9f1e50e1747610f5487a423b7ad3f5
Ilja Rotar <kontakt@ilja-rotar.de>
2025-12-10 17:30:28 +0100

    fader works

--- module/fader.go
+++ module/fader.go
@@ -12,19 +12,21 @@ func (f *fader) initialize(duration, sampleRate float64) {
 	delta := f.target - f.current
 	if duration == 0 || sampleRate == 0 {
 		f.step = delta
+		return
 	}
 	f.step = delta / (duration * sampleRate)
 }
 
 func (f *fader) fade() float64 {
+	// technically, this case is covered below, but for efficiency we make an early return here
 	if f.current == f.target {
 		return f.current
 	}
 
-	new := f.current + f.step
-	if (f.target-f.current >= 0) != (f.target-new >= 0) {
+	f.current += f.step
+	if (f.current > f.target) == (f.step > 0) {
 		f.current = f.target
 	}
-	f.current = new
+
 	return f.current
 }
